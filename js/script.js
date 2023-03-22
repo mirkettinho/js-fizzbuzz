d67c651bcf8837c66be40712fb33bf099042fce5
mirkettinho <mirkodifranco30@gmail.com>
2023-03-22 17:34:07 +0100

    [add] bonus

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
 const limit = 100;
 
 const container = document.querySelector(".container");
+
+
 console.log(container)
 let text = "";
 
   // STAMPATO I NUMERI IN CONSOLE DA 1 A 100
-for (let i = 1; i <= 100; i++){
+for (let i = 1; i <= limit; i++){
     
-    text = i;
+      const div = document.createElement("div");
+      div.classList.add("box");
+      container.append(div)
+      
+      
+
+    text = i ;
 
     // multipli di 3
   if(!(i % 3)){
     text = "fizz";
+    div.classList.add("three")
     
   }
 
     // multipli di 5
   if(!(i % 5)){
     text = "buzz";
+    div.classList.add("five")
     
   }
 
@@ -26,5 +36,5 @@ for (let i = 1; i <= 100; i++){
     text = "fizz buzz";
     
   }
-  console.log(text)
+  div.append(text)
 }
